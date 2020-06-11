b96a33a85867f0ae1451692dd238afe5c14508be
Tom Price <tfprice@gmail.com>
2020-06-11 08:54:47 -0500

    Update getPulseSurveyReminderEmail.js

--- hg/getPulseSurveyReminderEmail.js
+++ hg/getPulseSurveyReminderEmail.js
@@ -1,2 +1,10 @@
 var $hgid = "" //Insert the hgid of the _Survey_ here. Related query is in the comments
-db.getCollection('Recurrence').find({EntityId : $hgid, Status: {$ne:'Archived'}, MethodName: 'SendPulseSurveyReminder'})
+db.Recurrence.find(
+  {EntityId : $hgid, 
+   Status: {$ne:'Archived'}, 
+   MethodName: 'SendPulseSurveyReminder'}
+);
+//
+db.Survey.find(
+  {"Template.PulseQuestions.Question": "The senior leaders at Premise Health inspire me."}
+)
