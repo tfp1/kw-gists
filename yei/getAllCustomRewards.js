f5cd2053e3529a044f8c189e9a44dfe58ffbde39
Tom Price <tfprice@gmail.com>
2020-07-15 12:56:07 -0500

    Update getAllCustomRewards.js
    Added some `$ifNull` operations. Added categories. concatenated the image URL. Though you can't include the ObjectId, so we still have to do the regexreplace in Sheets

--- yei/getAllCustomRewards.js
+++ yei/getAllCustomRewards.js
@@ -1,47 +1,53 @@
 db.items.aggregate(
-	[{
-		$match: {
-			is_custom: true,
-			pooling: false,
-			give_back: false,
-			visible: true,
-		}
-	}, {
-		$lookup: {
-			from: 'users',
-			localField: 'fulfiller_id',
-			foreignField: '_id',
-			as: 'fulfiller'
-		}
-	}, {
-		$lookup: {
-			from: 'companies',
-			localField: 'company_id',
-			foreignField: '_id',
-			as: 'companies'
-		}
-	}, {
-		$match: {
-			"companies.status": "active"
-		}
-	}, {
-		$lookup: {
-			from: 'groups',
-			localField: 'group_ids',
-			foreignField: '_id',
-			as: 'groups'
-		}
-	}, {
-		$project: {
-            _id:0,
-			name: 1,
-			description: 1,
-			image: 1,
-			points: 1,
-			quantity: 1,
-			fulfiller_email: "$fulfiller.notification_email",
-			subdomain: "$companies.subdomain",
-			groups: "$groups.name"
-		}
-	}]
+[{
+    $match: {
+    is_custom: true,
+    pooling: false,
+    give_back: false,
+    visible: true,
+}
+}, {
+$lookup: {
+    from: 'users',
+    localField: 'fulfiller_id',
+    foreignField: '_id',
+    as: 'fulfiller'
+}
+}, {
+$lookup: {
+    from: 'companies',
+    localField: 'company_id',
+    foreignField: '_id',
+    as: 'companies'
+}
+}, 
+{
+$match: {
+    "companies.status": "active",
+    "companies.subdomain":{$ne: "followmefancy"}
+}
+}, {
+$lookup: {
+    from: 'groups',
+    localField: 'group_ids',
+    foreignField: '_id',
+    as: 'groups'
+}
+},
+{$lookup: {    from: 'categories',    localField: 'category_ids',    foreignField: '_id',    as: 'categories'}},
+{
+$project: {
+
+    name: 1,
+    description: {$ifNull: ["$description","null"]},
+    _id:1,
+    image: { $ifNull: [{$concat: ["https://yei-production-images.s3.amazonaws.com/uploads/item/image/","oid/","$image"]},"null"]},
+    points: 1,
+    quantity: 1,
+    fulfiller_email: { $ifNull: [{$arrayElemAt :["$fulfiller.notification_email",0]},"null"]},
+    subdomain: { $ifNull: [{$arrayElemAt :["$companies.subdomain",0]},"null"]},
+    groups: { $ifNull: [{$arrayElemAt :["$groups.name",0]},"null"]},
+    categories: { $ifNull: [{$arrayElemAt :["$categories.name",0]},"null"]},
+}
+}]
 ).toArray()
