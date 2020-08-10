9405ec232c74b0abbab4e431e0c291b898e56eed
Tom Price <tfprice@gmail.com>
2020-08-10 10:53:32 -0500

    Update getAllCustomRewards.js

--- yei/getAllCustomRewards.js
+++ yei/getAllCustomRewards.js
@@ -1,6 +1,6 @@
 db.items.aggregate(
 [{
-    $match: {
+    $match: { // Our WHERE clause for objects in the items collection
     is_custom: true,
     pooling: false,
     give_back: false,
@@ -8,14 +8,14 @@ db.items.aggregate(
     deleted_at:null
 }
 }, {
-$lookup: {
+$lookup: { //A JOIN to users so we can get the object for users marked as fulfillers on an item
     from: 'users',
     localField: 'fulfiller_id',
     foreignField: '_id',
     as: 'fulfiller'
 }
 }, {
-$lookup: {
+$lookup: { //a JOIN to companies so we can get the subdomain
     from: 'companies',
     localField: 'company_id',
     foreignField: '_id',
@@ -23,35 +23,32 @@ $lookup: {
 }
 }, 
 {
-$match: {
+$match: { //a WHERE clause on the companies objects to get only active companies and exclude our test company
     "companies.status": "active",
     "companies.subdomain":{$ne: "followmefancy"}
 }
 }, {
-$lookup: {
+$lookup: { //a JOIN to groups so we get get the name & ObjectID for the 'who is allowed to see this item?' filter in GRS
     from: 'groups',
     localField: 'group_ids',
     foreignField: '_id',
     as: 'groups'
 }
 },
-{$lookup: {    from: 'categories',    localField: 'category_ids',    foreignField: '_id',    as: 'categories'}},
+{$lookup: {    from: 'categories',    localField: 'category_ids',    foreignField: '_id',    as: 'categories'}}, //a JOIN to Categories to get the string name
 {
-$project: {
-
-    name: 1,
-    description: {$ifNull: ["$description","null"]},
-    _id:1,
-    image: { $ifNull: [{$concat: ["https://yei-production-images.s3.amazonaws.com/uploads/item/image/","oid/","$image"]},"null"]},
-    points: 1,
-    quantity: 1,
-    fulfiller_email: { $ifNull: [{$arrayElemAt :["$fulfiller.notification_email",0]},"null"]},
-    subdomain: { $ifNull: [{$arrayElemAt :["$companies.subdomain",0]},"null"]},
-    //groups: { $ifNull: [{$arrayElemAt :["$groups.name",0]},"null"]},
-    //group_id: { $ifNull: [{$arrayElemAt :["$groups._id",0]},"null"]},
-    groups: { $ifNull: ["$groups.name","null"]},
-    group_id: { $ifNull: ["$groups._id","null"]},
-    categories: { $ifNull: [{$arrayElemAt :["$categories.name",0]},"null"]},
+$project: { // This is where we select the fields that we want returned.
+    name: 1, //Name of the item
+    description: {$ifNull: ["$description",""]}, //Description of the item, but we pass an empty string to the JSON object if it's NULL so we are still able to convert to CSV
+    _id:1, //ObjectId of the item. Normally we'd want to stringify this in the $project pipeline, but Mongo 3.6.x doesn't support that. We resolve this later in a text editor using a RegEx Find & Replace
+    image: { $ifNull: [{$concat: ["https://yei-production-images.s3.amazonaws.com/uploads/item/image/","oid/","$image"]},"null"]}, //The URL of item's image. We need to put the ObjectID in the URL, but we can't do that in Mongo 3.6.x, so we get both values separately and then do a regexReplace in excel/sheets
+    points: 1, //Cost of the item in points
+    quantity: 1, //The quantity on hand. GRS doesn't really care about this but I never removed it from the query
+    fulfiller_email: { $ifNull: [{$arrayElemAt :["$fulfiller.notification_email",0]},"null"]}, //The email address of the FIRST fulfiller. $fulfiller.notification_email returns a list of all fulfillers on this item, but $arrayElemAt selects the 0 indexed item. We didn't want to deal with passing dozens of fulfillers.
+    subdomain: { $ifNull: [{$arrayElemAt :["$companies.subdomain",0]},"null"]}, //Subdomain of the company. We don't really need the $ifNull statement, but I was trying to be defensive
+    groups: { $ifNull: ["$groups.name","null"]}, //A list of the names of groups who are allowed to access this item
+    group_id: { $ifNull: ["$groups._id","null"]}, //A list of the IDs of groups who are allowed to access this item
+    categories: { $ifNull: [{$arrayElemAt :["$categories.name",0]},"null"]}, //The FIRST category of the item.
 }
 }]
 ).toArray()
