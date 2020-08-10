7beb990dd1fd7a68208a994af3e0f64985bba4aa
Tom Price <tfprice@gmail.com>
2020-08-10 13:45:17 -0500

    Update getAllCustomRewards.js
    I did add `group_constraint_name` in a related query, but I didn't save it. 🤷 
    
    Required a `$lookup` from `group_constraints` and then adding `$group_constraints.name` to the `$project` pipeline
    
    I wrote this after the fact and I _think_ it works, but not positive

--- yei/getAllCustomRewards.js
+++ yei/getAllCustomRewards.js
@@ -36,7 +36,8 @@ $lookup: { //a JOIN to groups so we get get the name & ObjectID for the 'who is 
 }
 },
 {$lookup: {    from: 'categories',    localField: 'category_ids',    foreignField: '_id',    as: 'categories'}}, //a JOIN to Categories to get the string name
-{
+{$lookup: {    from: 'group_constraints',    localField: 'category_ids',    foreignField: '_id',    as: 'group_constraints'}},
+ {
 $project: { // This is where we select the fields that we want returned.
     name: 1, //Name of the item
     description: {$ifNull: ["$description",""]}, //Description of the item, but we pass an empty string to the JSON object if it's NULL so we are still able to convert to CSV
@@ -44,11 +45,12 @@ $project: { // This is where we select the fields that we want returned.
     image: { $ifNull: [{$concat: ["https://yei-production-images.s3.amazonaws.com/uploads/item/image/","oid/","$image"]},"null"]}, //The URL of item's image. We need to put the ObjectID in the URL, but we can't do that in Mongo 3.6.x, so we get both values separately and then do a regexReplace in excel/sheets
     points: 1, //Cost of the item in points
     quantity: 1, //The quantity on hand. GRS doesn't really care about this but I never removed it from the query
-    fulfiller_email: { $ifNull: [{$arrayElemAt :["$fulfiller.notification_email",0]},"null"]}, //The email address of the FIRST fulfiller. $fulfiller.notification_email returns a list of all fulfillers on this item, but $arrayElemAt selects the 0 indexed item. We didn't want to deal with passing dozens of fulfillers.
-    subdomain: { $ifNull: [{$arrayElemAt :["$companies.subdomain",0]},"null"]}, //Subdomain of the company. We don't really need the $ifNull statement, but I was trying to be defensive
-    groups: { $ifNull: ["$groups.name","null"]}, //A list of the names of groups who are allowed to access this item
-    group_id: { $ifNull: ["$groups._id","null"]}, //A list of the IDs of groups who are allowed to access this item
-    categories: { $ifNull: [{$arrayElemAt :["$categories.name",0]},"null"]}, //The FIRST category of the item.
+    fulfiller_email: { $ifNull: [{$arrayElemAt :["$fulfiller.notification_email",0]},""]}, //The email address of the FIRST fulfiller. $fulfiller.notification_email returns a list of all fulfillers on this item, but $arrayElemAt selects the 0 indexed item. We didn't want to deal with passing dozens of fulfillers.
+    subdomain: { $ifNull: [{$arrayElemAt :["$companies.subdomain",0]},""]}, //Subdomain of the company. We don't really need the $ifNull statement, but I was trying to be defensive
+    groups: { $ifNull: ["$groups.name",""]}, //A list of the names of groups who are allowed to access this item
+    group_id: { $ifNull: ["$groups._id",""]}, //A list of the IDs of groups who are allowed to access this item
+    group_constraints_name:{ $ifNull: ["$group_constraints.name",""]},
+    categories: { $ifNull: [{$arrayElemAt :["$categories.name",0]},""]}, //The FIRST category of the item.
 }
 }]
 ).toArray()
