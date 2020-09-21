a6c69a47fa0708eb895e5a2136b59bdf695d98e2
Tom Price <tfprice@gmail.com>
2020-09-21 09:56:45 -0500

    Update getAllOrdersThatFailed.js

--- yei/getAllOrdersThatFailed.js
+++ yei/getAllOrdersThatFailed.js
@@ -1,15 +1,14 @@
-db.grs_orders.aggregate(
-{$match: {created_at: {$gte: ISODate("2020-09-15T18:00:00.000Z"),$lt: ISODate("2020-09-18T20:00:00.000Z")}}},
+db.grs_orders.aggregate([{
+$match: {created_at: {$gte: ISODate("2020-09-15T18:00:00.000Z"),$lt: ISODate("2020-09-18T20:00:00.000Z")}}},
 {$lookup: {from: 'companies',localField: 'company_id',foreignField: '_id',as: 'companies'}},
-{$lookup: {from: 'grs_line_items',localField: '_id',foreignField: 'grs_order_id',as: 'grs_line_items'}},
+{$lookup: {from: 'grs_line_items',localField: '_id',foreignField: 'grs_order_id',as: 'grs_line_items'}}, 
 {$project: {
-    _id: 0,
-    member_pin: "$grs_order_pin",
-    subdomain: {$arrayElemAt: ["$companies.subdomain", 0]},
-    partner_order_number: "$order_number",
-    total_point_cost: 1,
-    created_at: 1,
-    count_of_items: {$size: "$grs_line_items"}
-}}
-)
-//.toArray()
+        _id: 0,
+        member_pin: "$grs_order_pin",
+        subdomain: {$arrayElemAt: ["$companies.subdomain", 0]},
+        partner_order_number: "$order_number",
+        total_point_cost: 1,
+        created_at: 1,
+        count_of_items: {$size: "$grs_line_items"}
+    }
+}]).toArray()
