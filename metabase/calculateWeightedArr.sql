628b88d579611259d64db594c58063a2b2480ca8
Tom Price <tfprice@gmail.com>
2020-09-22 10:11:29 -0500

    Create calculateWeightedArr.sql

---
+++ metabase/calculateWeightedArr.sql
@@ -0,0 +1,12 @@
+select sum(`ARR` * `percent_slack_posts`)
+from (
+select
+(count(CASE when `cdc_prod.RR_posts`.`source` = "slack" then 1 else null end) / count(`cdc_prod.RR_posts`.`_id`)) as `percent_slack_posts`,
+`cdc_prod.sf_account`.`arr__c` as `ARR`
+from
+`cdc_prod.RR_posts`
+join `cdc_prod.RR_companies` on `cdc_prod.RR_posts`.`company_id` = `cdc_prod.RR_companies`.`_id`
+join `cdc_prod.sf_account` on  `cdc_prod.sf_account`.`YEI_Domain__c` = `cdc_prod.RR_companies`.`subdomain`
+where `cdc_prod.RR_posts`.`created_at` > {{Date}}
+GROUP by 2
+having count(CASE when `cdc_prod.RR_posts`.`source` = "slack" then 1 else null end) > 0)
